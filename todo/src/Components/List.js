08d61fd3dfd7909573fda9556cbd2e03dd09c850
syedaCodes <syedaa.codes@gmail.com>
2021-03-09 21:53:44 +0530

    stage 2

--- todo/src/Components/List.js
+++ todo/src/Components/List.js
@@ -1,65 +1,60 @@
 import React from 'react';
-
 import Icons from '../assets/sprite.svg';
 
 class List extends React.Component {
 
-    state = {
-        addClicked: true,
-        inputState: '',
-        data: []
-    }; //Constructor
+    constructor(props){
+        super(props);
+        this.state = {
+            addClicked: true,
+            inputState: '',
+            data: []
+        }; 
+
+        this.onInputChange = this.onInputChange.bind(this);
+    }
 
     onInputChange(event){
         event.preventDefault();
-        console.log(this.state.inputState);
-        console.log(event.target.elements);
-       // const items = e.target.elements.item.value.trim(); 
-
-        /**
-         * let arrayItems = [...this.state.data];
-    
-        if(this.state.inputState !== ''){
-            
-            arrayItems.push(this.state.inputState);
-            this.setState({addClicked: true, data: arrayItems });
-        }
-         */
+       // const text = this.state.inputState;
+        //console.log(text);
+        this.setState(state => {
+            const data = state.data.concat(this.state.inputState);
+            return {
+                data,
+                inputState: '',
+            };
+        });
+        console.log(this.state.data);
     }
+
+    handleOnChange(event){
+        event.preventDefault();
+    }
+
 
     render(){
 
         return(
             <div className="list">
-                {this.state.addClicked ? <button className="plusDiv" onClick={() => this.setState({addClicked: false})}>
+                {this.state.addClicked ?
+                <button className="plusDiv" onClick={e => {
+                    e.preventDefault();
+                    this.setState({addClicked: false})
+                }}>
                     <svg className="plusBtn"><use xlinkHref={`${Icons}#icon-plus`}></use></svg>
-                </button> : <form className="replaceDiv" onSubmit={this.onInputChange}>
-            <input type="text" className="toDoField" autoComplete="off" name="item" value={this.state.inputState} onChange={(e) => this.setState({inputState: e.target.value})}/>
-            <button className="checkDiv" >
-            <svg className="saveBtn"><use xlinkHref={`${Icons}#icon-check`}></use></svg>
-            </button>
-        </form>}
-        </div>
+                </button> :
+                <form className="replaceDiv" onSubmit={e => this.handleOnChange(e)}>
+                    <input type="text" className="toDoField" autoComplete="off" name="item" value={this.state.inputState} onChange={(e) => this.setState({inputState: e.target.value})}/>
+                    <button className="checkDiv" onClick={this.onInputChange}>
+                        <svg className="saveBtn"><use xlinkHref={`${Icons}#icon-check`}></use></svg>
+                    </button>
+                </form>}
+                {(this.state.data).length > 0 ? (this.state.data).map(item => <li className="item" key={item}>{item}</li>): null}
+            </div>
         );
     }
     
 }
 
 export default List;
-
-/**
- *  {this.state.itemContent? this.state.itemContent : 'text'}
-
- * <input type="text" className="toDoField" value={this.state.itemContent} onChange={e => this.setState({itemContent: e.target.value})} />
- <div className="checkDiv">
-                    <svg className="saveBtn"><use xlinkHref={`${Icons}#icon-check`}></use></svg>
-                </div>
- * <ul>
-                    
-                    <li className="item"><input type="checkbox" className="checkInput" />
-                    <a className="itemDone"><svg className="itemSvg done"><use xlinkHref={`${Icons}#icon-check`}></use></svg></a>
-                    <a className="itemContent">abc</a>
-                    <a className="itemDelete"><svg className="itemSvg del"><use xlinkHref={`${Icons}#icon-trash-o`}></use></svg></a>
-                    </li>
-                </ul>
- */
